044cb39c88c681889c79e107080aabb5a5b5fd73
Dell <Dell@DESKTOP-01BKPDG>
2020-06-09 03:26:04 +0700

    Add project files.

---
+++ WordAssembly/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
